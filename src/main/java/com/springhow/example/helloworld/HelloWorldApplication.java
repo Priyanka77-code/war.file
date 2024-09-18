bc77ffc5fb51c12e161e5077f3c41724b01eeb64
Priyanka77-code <priyankarouth2005@gmail.com>
2024-09-18 20:48:59 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello class!";
+        return "Hello Priyanka!";
     }
 
 }
